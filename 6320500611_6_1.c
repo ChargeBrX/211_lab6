bfbef7d85cdad8b4d53b27570f3de8b37f0ab87f
ChargeBrX <panuwat.janj@ku.th>
2021-03-18 16:44:13 +0700

    sort 1

--- 6320500611_6_1.c
+++ 6320500611_6_1.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype>
 int main()
 {
     int n,i,j;
     scanf("%d",&n);
 
-    char a[n][1000],p[1000];
+    char a[n][1000],p[1000],c,b[n];
 
-    for(i=0;i<n;i++)
+    for(i=0,j=n-1; i<n; i++,j--)
     {
         scanf("%s",a[i]);
+        b[j] = a[i][0];
+        c = toupper(a[i][0]);
+        a[i][0] = c;
+
     }
 
-    for(i=1;i<n;i++)
+    for(i=0; i<n; i++)
     {
-        if(strcmp(a[i-1],a[i])>0)
+        for(j=0; j<n-i; j++)
         {
-            strcpy(p,a[i]);
-            strcpy(a[i],a[i-1]);
-            strcpy(a[i-1],p);
+            if(strcmp(a[j],a[j+1])<0)
+            {
+                strcpy(p,a[j+1]);
+                strcpy(a[j+1],a[j]);
+                strcpy(a[j],p);
+            }
         }
     }
-    for(i=0;i<n;i++)
+    for(i=n-1; i>=0; i--)
     {
+        a[i][0] = b[i];
         printf("%s\n",a[i]);
     }
 }
